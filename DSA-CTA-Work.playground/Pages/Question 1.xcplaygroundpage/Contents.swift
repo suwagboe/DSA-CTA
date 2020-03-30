31e14783534f1ac661dcea077271f8431252204b
uwagboe <sm.uwagboe@gmail.com>
2020-03-30 10:51:44 -0400

    added playground file to see questions work

---
+++ DSA-CTA-Work.playground/Pages/Question 1.xcplaygroundpage/Contents.swift
@@ -0,0 +1,17 @@
+// Question 1 :
+// Q1.
+// Given a linked list, write a function to traverse it and print each value in order.
+
+class Node<T> {
+    var next: Node?
+    let val: T
+    init(val: T) {
+        self.val = val
+    }
+}
+
+// Print format:
+
+```
+-> 4 -> 9 -> 2 -> 3 -> nil
+```
